10ae97b27e55cb030f773e234e96be2c480f4bda
huwenboshi <hshi@hsph.harvard.edu>
2019-07-12 19:22:18 -0400

    added example 2

---
+++ chp9/eg2_graph.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+x1 = tf.Variable(1)
+print(x1.graph is tf.get_default_graph())
+
+graph = tf.Graph()
+with graph.as_default():
+    x2 = tf.Variable(2)
+print(x2.graph is graph)
+print(x2.graph is tf.get_default_graph())
