ac63b095f6d57e19774c32a82b7374c64a5e4271
Huwenbo Shi <hshi@hsph.harvard.edu>
2019-07-12 20:39:11 -0400

    added scripts to perform automatic gradient and optimization

---
+++ chp9/eg5_autooptim.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+
+n_epochs = 1000
+learning_rate = 0.01
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random.uniform([n+1, 1], -1.0, 1.0), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients = tf.gradients(mse, [theta])[0]
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.compat.v1.global_variables_initializer()
+with tf.compat.v1.Session() as sess:
+    sess.run(init)
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+
+    best_theta = theta.eval()
+
+print(best_theta)
